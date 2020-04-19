88ca976015b6cbd6606f7ba95100a29f30aa2abe
hayley luke <jhluke@uw.edu>
2020-04-19 15:38:20 -0700

    figure out AQUAINT path

--- src/data_input.py
+++ src/data_input.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import xml.etree.ElementTree as ET
 
 """Where data is inputted"""
@@ -9,13 +10,37 @@ import xml.etree.ElementTree as ET
 __author__ = 'Daniel Campos, Sicong Huang, Hayley Luke, Simola Nayak, Shunjie Wang  '
 __email__ = 'dacampos@uw.edu'
 
-AQUAINT_ROOT = './patas/573/AQUAINT'
-AQUAINT2_ROOT = './patas/573/AQUAINT-2'
+AQUAINT2_ROOT = './patas/AQUAINT-2'
 DATA_ROOT = './patas/573/Data'
 
+# examples:
+# <doc id = "NYT19980903.0137" />
+# /corpora/LDC/LDC02T31/nyt/1998/19980903_NYT
+PATH_MAPPING = {
+    'AQUAINT': {
+        # Do some regex matching to build a path
+        # (tag)(year)(group id).(doc id)
+        'regex': re.compile(r'^([A-Z]{3})([0-9]{4})([0-9]{4})\.([0-9]{4})$'),
+        'path': '{tag_lower}/{year}/{year}{group_id}_{tag}_ENG',
+        'root': './patas/AQUAINT'
+    }
+}
 
-def build_path(topic_id, doc_id):
-    pass
+
+def build_path(doc_id):
+    for corpus, info in PATH_MAPPING.items():
+        match = info['regex'].match(doc_id)
+
+        if not match:
+            continue
+
+        tag, year, group_id, doc_id = match.groups()
+        path = info['path'].format(tag=tag, tag_lower=tag.lower(), year=year, group_id=group_id, doc_id=doc_id)
+        return os.path.join(info['root'], path)
+
+    # If we get here without returning, we didn't figure out the path
+    print('Unable to find path for {doc_id}'.format(doc_id))
+    return None
 
 
 class Topic:
@@ -27,8 +52,21 @@ class Topic:
         self.documents = []
 
     def load_doc(self, doc_id):
-        path = build_path(self.id, doc_id)
-        import ipdb; ipdb.set_trace()
+        path = build_path(doc_id)
+        with open(path, 'r') as f:
+            contents = f.read()
+
+        # Do some basic escaping, and add a root node
+        # This is all very hacky, but the xml is extremely poorly formatted...
+        # Get rid of amperstands entirely (maybe change in the future?)
+        contents = contents.replace('&AMP;', '')
+        contents = '<root>' + contents + '</root>'
+        group_tree = ET.fromstring(contents)
+
+        for child in group_tree.findall('DOC'):
+            found_doc_id = child.find('DOCNO').text.strip()
+            if doc_id == found_doc_id:
+                import ipdb; ipdb.set_trace()
 
 
 def get_topics(path_to_topic):
