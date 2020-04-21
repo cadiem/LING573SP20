2546fe8db34552e19287ace87c0ac76170696d7f
Sicong Huang <huangs33@patas.ling.washington.edu>
2020-04-21 15:38:39 -0700

    bug fix in run_rouge.py

--- ROUGE/run_rouge.py
+++ ROUGE/run_rouge.py
@@ -9,7 +9,8 @@ def parse_args():
 def main():
     args = parse_args()
     rouge_data = '/dropbox/19-20/573/code/ROUGE/data'
-    command = f'-e {rouge_data} -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d {args.config}'
+    perl_file = '/dropbox/19-20/573/code/ROUGE/ROUGE-1.5.5.pl'
+    command = 'perl {} -e {} -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d {}'.format(perl_file, rouge_data, args.config)
     os.system(command)
 
 if __name__ == '__main__':
