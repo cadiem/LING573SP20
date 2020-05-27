aabfae563ec9aeed2439b4193443303e0683a288
Simola Nayak <simola.nayak@gmail.com>
2020-05-26 20:37:45 -0400

    stubs for methods.

--- src/content_realization.py
+++ src/content_realization.py
@@ -8,13 +8,26 @@ __email__ = 'dacampos@uw.edu,  huangs33@uw.edu, shunjiew@uw.edu, simnayak@uw.edu
 import sys
 import os
 
-#baseline implementation
-#takes sentences in and writes them line-by-line to an output file
+def combine_sentences(sentence_collection):
+    #combine sentences for clarity, etc
+    return sentence_collection
 
+def remove_gratuitous_nodes(sentence):
+    #remove extra branches of the sentence parse tree
+    return sentence
+
+def pre_clean(sentence_collection):
+    #use regexes to remove the bylines, datelines, etc.
+    return sentence_collection
+    
 def realize_content(topics, summaries, output_dir, run_id):
     for topic in topics:
         print(topic.id)
         filename = '{}-A.M.100.{}.{}'.format(topic.id_1, topic.id_2, run_id)
         with open(os.path.join(output_dir, filename), 'w') as w:
+            pre_clean(summaries[topic.id])
+            for sentence in summaries[topic.id]:
+                sentence = remove_gratuitous_nodes(sentence)
+            summaries[topic.id] = combine_sentences(summaries[topic.id])
             for sentence in summaries[topic.id]:
                 w.write("{}\n".format(sentence.text))
