a48e92c89d856525ca11d1954200a74d75710f62
Simola Nayak <simola.nayak@gmail.com>
2020-05-29 13:53:21 -0400

    more work

--- src/content_realization.py
+++ src/content_realization.py
@@ -9,18 +9,23 @@ import sys
 import os
 import re
 
+def shrink_corefs(sentence_collection):
+    return sentence_collection
+
 def combine_sentences(sentence_collection):
     #combine sentences for clarity, etc
     return sentence_collection
 
-def remove_gratuitous_nodes(sentence):
+def remove_gratuitous_nodes(sentence_collection):
     #remove extra branches of the sentence parse tree
-    return sentence
+    for sentence in sentence_collection:
+        pass
+        #TODO Parse and hack 
+    return sentence_collection
 
 def pre_clean(sentence_collection):
     #use regexes to remove the bylines, datelines, etc.
-    re.sub("[A-Z],","", sentence_collection)
-    
+    re.sub("[A-Z]*,","", sentence_collection)
     return sentence_collection
     
 def realize_content(topics, summaries, output_dir, run_id):
@@ -29,8 +34,8 @@ def realize_content(topics, summaries, output_dir, run_id):
         filename = '{}-A.M.100.{}.{}'.format(topic.id_1, topic.id_2, run_id)
         with open(os.path.join(output_dir, filename), 'w') as w:
             pre_clean(summaries[topic.id])
-            for sentence in summaries[topic.id]:
-                sentence = remove_gratuitous_nodes(sentence)
-            summaries[topic.id] = combine_sentences(summaries[topic.id])
+            shrink_corefs(summaries[topic.id])
+            remove_gratuitous_nodes(summaries[topic.id])
+            combine_sentences(summaries[topic.id])
             for sentence in summaries[topic.id]:
                 w.write("{}\n".format(sentence.text))
