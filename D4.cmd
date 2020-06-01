fb622a6794a29668a1d0925d07a1eed9824a5bb5
Your Name <you@example.com>
2020-05-31 22:12:07 -0700

    getting things ready for final submission

--- D4.cmd
+++ D4.cmd
@@ -1,6 +1,7 @@
 universe = vanilla
 executable = /bin/bash
 getenv = true
+error = err
 output = acc_file
 log = log
 arguments = "./run.sh"
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source /home2/jhluke/LING573SP20/venv/bin/activate #please activate you ling573 environment accordingly
-python src/main.py --corpus_config /dropbox/19-20/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml --results_path results/D4_rouge_scores.out  --model_dir /dropbox/19-20/573/Data/models/devtest/ --output_dir outputs/D4/ --do_summarize 1
+python src/main.py --corpus_config /dropbox/19-20/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml --results_path results/D4_devtest_rouge_scores.out  --model_dir /dropbox/19-20/573/Data/models/devtest/ --output_dir outputs/D4_devtest/ --do_summarize 1
+python src/main.py --corpus_config /dropbox/19-20/573/Data/Documents/evaltest/GuidedSumm11_test_topics.xml --results_path results/D4_evaltest_rouge_scores.out  --model_dir /dropbox/19-20/573/Data/models/evaltest/ --output_dir outputs/D4_evaltest/ --do_summarize 1
+
